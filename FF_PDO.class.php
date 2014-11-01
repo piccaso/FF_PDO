5b912f182a682d1f539a1292283c91e2a0f1d42c
Flo <flo@cdc5.fida.biz>
2014-11-01 17:14:05 +0100

    init

---
+++ FF_PDO.class.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Class FF_PDO
+ */
+class FF_PDO extends PDO {
+    /**
+     * For compatibility with SQLITE3
+     * @param $sql
+     * @return array|bool|mixed
+     */
+    public function querySingle($sql){
+        $r = false;
+        $res = $this->query($sql);
+        if($res){
+            $r = $res->fetch();
+            $res->closeCursor();
+            if($r !== false AND is_array($r) AND count($r) === 1) $r = current($r);
+        }
+        return $r;
+    }
+
+    private function build_args($cnt,$all_args){
+        if(is_array($all_args)) {
+            if($cnt == 2 && is_array($all_args[1])){
+                return $all_args[1];
+            }
+            if ($cnt > 1) {
+                array_shift($all_args);
+                return ($all_args);
+            }
+        }
+        return array();
+    }
+
+    private function is_valid_column_name($column_name){
+        return preg_match('/^[a-zA-Z_][a-zA-Z0-9_]*$/',$column_name);
+    }
+
+    public function q($sql,$args=null){
+        $args = $this->build_args(func_num_args(),func_get_args());
+        $ps = $this->prepare($sql);
+        if($ps->execute($args)){
+            $r = $ps->fetch();
+            if($ps->columnCount() === 1){
+                $r=current($r);
+            }
+            $ps->closeCursor();
+            return $r;
+        }
+    }
+
+    public function qt($sql,$args=null){
+        $args = $this->build_args(func_num_args(),func_get_args());
+        $ps = $this->prepare($sql);
+        if($ps->execute($args)){
+            $r = $ps->fetchAll();
+            $ps->closeCursor();
+            return $r;
+        }
+    }
+
+    public function qv($sql,$args=null){
+        $args = $this->build_args(func_num_args(),func_get_args());
+        $ps = $this->prepare($sql);
+        if($ps->execute($args)){
+            $r = array();
+            while($row = $ps->fetch()){
+                $r[]=current($row);
+            }
+            $ps->closeCursor();
+            return $r;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
